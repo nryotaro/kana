3dc4798c8873fd631d35b3b69448261ddf27566f
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2021-07-26 11:16:06 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use gtk::prelude::*;
+use kana::persistence::samba::SambaClient;
 use kana::port;
-use kana::persistence::samba::{SambaClient};
-
-
 
 fn main() {
-
-    let a: Option<Box<SambaClient>> = port::DocumentRepository::new("");
+    let a: Option<Box<SambaClient>> = port::DocumentRepository::new("smb://192.168.1.2/share/documents/manga/");
+    match a {
+        Some(aa) => println!("found"),
+        None => println!("fail"),
+    }
     /*
     let configuration = persistence::config::load_config();
     let application = ui::initialize();
--- src/persistence/samba.rs
+++ src/persistence/samba.rs
@@ -1,5 +1,6 @@
 mod wrapper;
 use crate::port::DocumentRepository;
+use std::ffi::CString;
 use std::os::raw::{c_char, c_int, c_void};
 
 pub struct SambaClient {
@@ -9,37 +10,53 @@ pub struct SambaClient {
 
 impl DocumentRepository for SambaClient {
 	fn new(url: &'static str) -> Option<Box<Self>> {
-		let smbcctx = unsafe {
+		let smbcctx: *mut wrapper::_SMBCCTX = unsafe {
 			let smbcctx = wrapper::smbc_new_context();
 			if smbcctx.is_null() {
-				()
+				return None;
 			}
 			wrapper::smbc_setFunctionAuthDataWithContext(smbcctx, Some(helper));
 			if wrapper::smbc_init_context(smbcctx).is_null() {
 				wrapper::smbc_free_context(smbcctx, 0);
-				()
+				return None;
 			}
-			Some(smbcctx)
+			// いらないかも
+			wrapper::smbc_set_context(smbcctx);
+			smbcctx
 		};
-		match smbcctx {
-			Some(context) => Some(Box::new(SambaClient {
-				smbcctx: context,
-				url: url.to_string(),
-			})),
-			None => None,
-		}
+		unsafe {
+			let path = CString::new(url).unwrap();
+			let fd = wrapper::smbc_getFunctionOpendir(smbcctx).unwrap()(smbcctx, path.as_ptr());
+			println!("doge");
+			if fd.is_null() {
+				close_smbcctx(smbcctx);
+				return None;
+			}
+		};
+
+		Some(Box::new(SambaClient {
+			smbcctx: smbcctx,
+			url: url.to_string(),
+		}))
+	}
+}
+
+extern "C" fn close_smbcctx(smbcctx: *mut wrapper::_SMBCCTX) {
+	unsafe {
+		wrapper::smbc_getFunctionPurgeCachedServers(smbcctx).unwrap()(smbcctx);
+		wrapper::smbc_free_context(smbcctx, 1);
 	}
 }
 
 extern "C" fn helper(
-	c: *mut wrapper::_SMBCCTX,
-	server: *const c_char,
-	share: *const c_char,
-	workgroup: *mut c_char,
-	workgroup_length: c_int,
-	username: *mut c_char,
-	username_length: c_int,
-	password: *mut c_char,
-	password_length: c_int,
+	_context: *mut wrapper::_SMBCCTX,
+	_server: *const c_char,
+	_share: *const c_char,
+	_workgroup: *mut c_char,
+	_workgroup_length: c_int,
+	_username: *mut c_char,
+	_username_length: c_int,
+	_password: *mut c_char,
+	_password_length: c_int,
 ) {
 }
